ea8874edb0a85f3e44b46d0c7942323d9811a348
HO3HSIYAH <46374084+hosh3iyah@users.noreply.github.com>
2024-08-21 20:50:53 +0330

    Update template.inc.php

--- template.inc.php
+++ template.inc.php
@@ -12,7 +12,7 @@
  */
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fa" dir="rtl">
 <head>
   <link type="text/css" rel="stylesheet" href="<?= $APP->PATH ?>helpers/materialize-1.0.0/css/materialize.min.css" media="screen,projection"/>
   <link type="text/css" rel="stylesheet" href="<?= $APP->PATH ?>helpers/material-icons-1.13.6/css/material-icons.css" media="screen,projection"/>
@@ -20,8 +20,8 @@
   <link type="text/css" rel="stylesheet" href="<?= $APP->PATH ?>helpers/easymde-2.16.1/css/easymde.min.css" media="screen,projection"/>
   <link type="text/css" rel="stylesheet" href="<?= $APP->PATH ?>helpers/highlightjs-11.9.0/css/<?= ($APP->DARK?"monokai-sublime":"default") ?>.min.css" media="screen,projection">
   <link type="text/css" rel="stylesheet" href="<?= $APP->PATH ?>helpers/katex-0.16.10/katex.min.css" media="screen,projection">
-  <link type="text/css" rel="stylesheet" href="<?= $APP->PATH ?>styles/styles-rtl.css" media="screen,projection"/>
-  <link type="text/css" rel="stylesheet" href="<?= $APP->PATH ?>styles/styles-<?= ($APP->DARK?"dark":"light") ?>-rtl.css" media="screen,projection"/>
+  <link type="text/css" rel="stylesheet" href="<?= $APP->PATH ?>styles/styles.css" media="screen,projection"/>
+  <link type="text/css" rel="stylesheet" href="<?= $APP->PATH ?>styles/styles-<?= ($APP->DARK?"dark":"light") ?>.css" media="screen,projection"/>
   <?php if(file_exists($APP->DIR."styles/styles-custom.css")): ?><link type="text/css" rel="stylesheet" href="<?= $APP->PATH ?>styles/styles-custom.css" media="screen,projection"/><?php echo "\n"; endif; ?>
   <link rel="icon" type="image/x-icon" href="<?= $APP->PATH ?>favicon.ico" sizes="any">
   <link rel="icon" type="image/png" sizes="512x512" href="<?= $APP->PATH ?>favicon-512x512.png">
